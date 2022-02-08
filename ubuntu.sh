7e05cd2489e5806fa67c827848200139bd44e4ff
Nia Watts <niawatts95@gmail.com>
2022-02-08 13:28:48 -0700

    Add updated docker compose

--- ubuntu.sh
+++ ubuntu.sh
@@ -19,6 +19,10 @@ if [[ ! -x "$(command -v docker)" ]]; then
   # Add user to docker groud
   sudo groupadd docker
   sudo usermod -aG docker "$USER"
+
+  mkdir -p ~/.docker/cli-plugins/
+  curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+  chmod +x ~/.docker/cli-plugins/docker-compose
 fi
 
 if [[ ! -x "$(command -v kubectl)" ]]; then
