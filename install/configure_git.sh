7e02ff95e9be15b67f6f0d581c9450917e82b2e8
Nia Watts <niawatts95@gmail.com>
2022-07-29 16:41:34 -0600

    Add git restore commands

--- install/configure_git.sh
+++ install/configure_git.sh
@@ -11,6 +11,8 @@ git config --global init.defaultBranch 'main'
 # Aliases
 git config --global alias.s 'switch'
 git config --global alias.sc 'switch -c'
+git config --global alias.r 'restore'
+git config --global alias.ra 'restore .'
 git config --global alias.co 'checkout'
 git config --global alias.cob 'checkout -b'
 git config --global alias.p 'push'
@@ -22,7 +24,6 @@ git config --global alias.d 'diff'
 git config --global alias.st 'status'
 git config --global alias.a 'add'
 git config --global alias.b 'branch'
-git config --global alias.r 'reset'
 git config --global alias.rhh 'reset --hard HEAD'
 
 # .gitignore
