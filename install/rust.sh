87b515821d0d9096e785db0385a3f005274f185f
Nia Watts <niawatts95@gmail.com>
2022-08-30 19:33:18 -0600

    Add cargo-license

--- install/rust.sh
+++ install/rust.sh
@@ -6,3 +6,6 @@ set -eou pipefail
 if [[ ! -x "$(command -v rustup)" ]]; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 fi
+
+# Install cargo-license
+cargo install cargo-license
