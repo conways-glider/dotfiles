8485f1b4124bb1ec6f70b7619871794bec4a25f2
Nia Watts <niawatts95@gmail.com>
2022-08-30 20:24:34 -0600

    Add cargo-deny

--- install/rust.sh
+++ install/rust.sh
@@ -7,5 +7,6 @@ if [[ ! -x "$(command -v rustup)" ]]; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 fi
 
-# Install cargo-license
-cargo install cargo-license
+# Install cargo tools
+cargo install --locked cargo-license
+cargo install --locked cargo-deny
