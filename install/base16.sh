17fc0a43560a6bcb61c4707a7778ce84af78930b
Nia Watts <niawatts95@gmail.com>
2022-12-06 15:25:06 -0700

    Update base16 to new organization

--- install/base16.sh
+++ install/base16.sh
@@ -5,4 +5,5 @@ set -eou pipefail
 # Install Base16 Shell
 echo "Installing Base16 Shell"
 rm -rf ~/.config/base16-shell
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+git clone https://github.com/tinted-theming/base16-shell.git \
+  $HOME/.config/base16-shell
