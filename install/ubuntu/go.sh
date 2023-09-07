03f68223e07790b12fc1fb637f952cb1b4117069
Nia Maxwell <niawatts95@gmail.com>
2023-09-07 01:42:53 -0600

    Remove useless profile lines

--- install/ubuntu/go.sh
+++ install/ubuntu/go.sh
@@ -13,10 +13,3 @@ sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ${BUNDLE_FILENAME}
 # Clean up go
 rm -rf ${BUNDLE_FILENAME}
 rm -rf ~/go
-
-# Add to .profile
-LINE='export PATH=$PATH:/usr/local/go/bin'
-BASH_FILE="${HOME}/.profile"
-ZSH_FILE="${HOME}/.zprofile"
-grep -qF -- "$LINE" "$BASH_FILE" || echo "$LINE" >> "$BASH_FILE"
-grep -qF -- "$LINE" "$ZSH_FILE" || echo "$LINE" >> "$ZSH_FILE"
