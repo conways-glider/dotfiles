405491af04ce001880da062c7a94de8a6fa6e122
Nia Watts <niawatts95@gmail.com>
2023-09-07 00:57:13 -0600

    Add sudo to go install

--- install/ubuntu/go.sh
+++ install/ubuntu/go.sh
@@ -8,7 +8,7 @@ echo "Installing go"
 GO_VERSION_TAG=$(curl "https://go.dev/VERSION?m=text" | head -1)
 BUNDLE_FILENAME="${GO_VERSION_TAG}.linux-amd64.tar.gz"
 wget "https://dl.google.com/go/${BUNDLE_FILENAME}"
-rm -rf /usr/local/go && tar -C /usr/local -xzf ${BUNDLE_FILENAME}
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ${BUNDLE_FILENAME}
 
 # Clean up go
 rm -rf ${BUNDLE_FILENAME}
