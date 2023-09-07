171cf4a66dd404736f249db9a2a2ca95dac6907c
Nia Watts <niawatts95@gmail.com>
2023-09-07 01:00:25 -0600

    Add removal of spaceship

--- install/ubuntu/spaceship.sh
+++ install/ubuntu/spaceship.sh
@@ -5,4 +5,6 @@ set -eou pipefail
 echo "Installing spaceship"
 
 mkdir -p "$HOME/.zsh"
+
+rm -rf ~/.zsh/spaceship
 git clone --depth=1 https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.zsh/spaceship"
