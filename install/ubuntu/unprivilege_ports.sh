23bcb96cff9dd5c87c243ae282670d1b317a9f65
Nia <niawatts95@gmail.com>
2023-10-06 13:13:25 -0600

    Update port script

--- install/ubuntu/unprivilege_ports.sh
+++ install/ubuntu/unprivilege_ports.sh
@@ -4,5 +4,5 @@ set -eou pipefail
 
 if [[ ! -x "$(cat /etc/sysctl.conf | grep \"net.ipv4.ip_unprivileged_port_start=0\")" ]]; then
   echo "Updating /etc/sysctl.conf"
-#   sysctl -w net.ipv4.ip_unprivileged_port_start=0 >>/etc/sysctl.conf
+  sudo sysctl -w net.ipv4.ip_unprivileged_port_start=0 | sudo tee -a /etc/sysctl.conf
 fi
