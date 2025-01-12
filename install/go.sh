6302a5d736986b53246233e71455849c421faef4
Nia <niawatts95@gmail.com>
2025-01-11 17:15:14 -0700

    Update golang ci lint

--- install/go.sh
+++ install/go.sh
@@ -7,4 +7,4 @@ echo "Installing go packages and binaries"
 go install github.com/google/go-licenses@latest
 go install github.com/antonmedv/fx@latest
 go install sigs.k8s.io/kind@v0.20.0
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.54.2
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.62.0
