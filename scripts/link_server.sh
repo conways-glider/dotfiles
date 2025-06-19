8db176cc8d6661ab9a03dab6b719fb047037561e
Nia <nia@conwaysglider.com>
2025-06-18 22:03:02 -0600

    Update server links

--- scripts/link_server.sh
+++ scripts/link_server.sh
@@ -10,6 +10,16 @@ ln -sf "$(pwd)"/zsh/zshrc ~/.zshrc
 echo "Making .config folder"
 mkdir -p ~/.config
 
+# Link zellij config
+echo "Linking zellij config"
+rm -rf ~/.config/zellij
+ln -sf "$(pwd)"/zellij ~/.config/zellij
+
+# Link fish config
+echo "Linking fish config"
+rm -rf ~/.config/fish
+ln -sf "$(pwd)"/fish ~/.config/fish
+
 # Link helix config
 echo "Linking helix config"
 rm -rf ~/.config/helix
