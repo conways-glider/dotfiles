f3283231a2b217e6854b8e76504c62f120fa5a78
Nia <nia@conwaysglider.com>
2025-09-18 23:02:55 -0600

    Write better fish tests

--- fish/conf.d/homebrew.fish
+++ fish/conf.d/homebrew.fish
@@ -1,12 +1,12 @@
 # Setup homebrew
 if test -x /opt/homebrew/bin/brew
     eval "$(/opt/homebrew/bin/brew shellenv)"
-end
 
-if test -d (brew --prefix)"/share/fish/completions"
-    set -p fish_complete_path (brew --prefix)/share/fish/completions
-end
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
 
-if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
 end
--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,6 @@
-function fish_user_key_bindings
-  fzf --fish | source
+# Only load fzf key bindings if fzf is installed
+if type -q fzf
+  function fish_user_key_bindings
+    fzf --fish | source
+  end
 end
