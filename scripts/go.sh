38833deb92cb70852007a75db47a98a04593f60e
Nia <nia@conwaysglider.com>
2025-09-19 14:06:54 -0600

    Add go to path

--- scripts/go.sh
+++ scripts/go.sh
@@ -2,6 +2,9 @@
 
 set -eou pipefail
 
+echo "Adding go to PATH temporarily"
+export PATH=$PATH:/usr/local/go/bin
+
 echo "Installing go packages and binaries"
 
 go install github.com/go-delve/delve/cmd/dlv@latest
